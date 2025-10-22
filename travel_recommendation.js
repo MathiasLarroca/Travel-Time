23b45e8c3b518406b004bfaffe623c484a8f1b19
MathiasLarroca <mathiaslarrocagonzalez@gmail.com>
2025-10-21 22:33:52 -0300

    Search event returns valid query

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -2,23 +2,34 @@ async function loadData() {
     const response = await fetch('travel_recommendation_api.json')
     return response.json();
 }
-//add variable to setupEventListeners function
-const SearchQuery = document.getElementById('search_text');
-const SearchBtn = document.getElementById('search_btn');
-const ClearBtn = document.getElementById('clear_btn');
 
 //use init to initialize other functions
 async function init() {
     const destinations = await loadData();
+    console.log(Object.keys(destinations));
     console.log(destinations);
+    eventListeners();
 }
 
-//add to setupEventListeners function as well
-SearchBtn.addEventListener('click', () => {
-    console.log(SearchQuery.value)
-    // for (let i = 0; i > destinations.length; i++) {
+function eventListeners() {
+    const SearchQuery = document.getElementById('search_text');
+    const SearchBtn = document.getElementById('search_btn');
+    const ClearBtn = document.getElementById('clear_btn');
 
-    // }
-})
+    SearchBtn.addEventListener('click', () => {
+        const validQueries = {
+            countries : [],
+            temples : ['temple', 'temples'],
+            beaches : ['beach', 'beaches'],
+        };
+        let query = SearchQuery.value.toLowerCase();
+        Object.keys(validQueries).forEach(key => {
+            if (validQueries[key].some((value) => value === query)) {
+                console.log(key)
+            }
+        });
+    })
+
+}
 
 init();
